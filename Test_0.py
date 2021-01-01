f65df789f7277e161750db9d63a71720b24ac8a7
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-01 14:32:31 +0900

    modified Test_0.py about image property

--- Test_0.py
+++ Test_0.py
@@ -3,6 +3,12 @@ import cv2
 # 이미지 읽기
 img = cv2.imread('images/test.jpg', cv2.IMREAD_COLOR)
 
+# 이미지 속성 확인
+height, width, channel = img.shape
+print(height, width, channel)
+
 # 이미지 띄우기
 cv2.imshow('Test Image', img)
 cv2.waitKey(0)  # 키값을 주지 않으면 바로 켜졌다 꺼짐
+
+
