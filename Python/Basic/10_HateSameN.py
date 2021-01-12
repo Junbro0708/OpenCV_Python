a2041ccc7d83cf50be657ed3a32e957a91428c8e
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-12 13:48:05 +0900

    this time, I did itgit add . but it's probably easy algorithm :(

---
+++ Python/Basic/10_HateSameN.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+
+    for i in range(0, len(arr) - 1, 1):
+        if arr[i] != arr[i + 1]:
+            answer.append(arr[i])
+    answer.append(arr[len(arr) - 1])
+    return answer
+
+
+print(solution([1, 1, 3, 3, 0, 1, 1]))
