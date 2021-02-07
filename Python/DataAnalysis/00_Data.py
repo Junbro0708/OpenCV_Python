bd967eeb108c83d393a72b790f1da069b48fad91
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-02-07 20:54:33 +0900

    data analysis

--- Python/DataAnalysis/00_Data.py
+++ Python/DataAnalysis/00_Data.py
@@ -16,10 +16,29 @@ print(s)
 
 # --------------------------------------- 데이터 프레임
 
-dates = pd.date_range('20210131', periods=6)  # 6일 동안의 날짜 범위를 만들어줌
-df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=['A', 'B', 'C', 'D'])
+dates = pd.date_range('20210131', periods=5)  # 6일 동안의 날짜 범위를 만들어줌
+df = pd.DataFrame(np.random.randn(5, 3), index=dates, columns=['A', 'B', 'C'])
 print(df.head(6))
 print(df.index)
 print(df.columns)
 print(df.info())
 print(df.describe())
+
+# --------------------------------------
+
+print(df.sort_values(by='B', ascending=True), end="\n\n")  # 정렬
+print(df[['A']])
+print(df[0:3])
+print(df.iloc[1:4, [1, 2]])
+print(df.iloc[:, :])
+print(df[df > 0])
+
+df2 = df
+print(df2)
+print(df2.sort_values(by='A', ascending=False))
+
+df2["순위"] = [1, 2, 3, 4, 5]
+print(df2)
+print(df2["순위"].isin([1, 3, 5]))
+print(df2.apply(np.cumsum))
+print(df2.apply(lambda x: x.max() - x.min()))
